b06dc098238f90ad7c4bd93c0def410f385d0042
Alexey Boyko <alexeyboiko@bk.ru>
2023-07-01 21:24:39 +0300

    adaptive for mobile

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 const swiper = new Swiper('.swiper', {
     speed: 400,
     loop: true,
-    slidesPerView: 2,
+    slidesPerView: 1,
     navigation: {
         nextEl: '.arrow',
     },
+    breakpoints: {
+        600: {
+            slidesPerView: 2,
+        }
+    }
 });
+
+const buttonMenu = document.querySelector('.menu-button');
+const headerMenu = document.querySelector('.header');
+buttonMenu.addEventListener('click', ()=> {
+    headerMenu.classList.toggle('header-active');
+    buttonMenu.classList.toggle('menu-button-active');
+})
